c6b318de17e7134664ebe2bc400fa1fa471ddce3
Nikola <nikola@lucky-kat.com>
2016-07-25 15:34:58 +0200

    don't use the 'apply plugin' part

--- hooks/android/after_plugin_install.js
+++ hooks/android/after_plugin_install.js
@@ -6,7 +6,7 @@ var path = require('path');
 // Check of gradle file that we are going to append to the main build.gradle
 var DATA = `
 // Added by plugin cordova-plugin-firebaseservice
-apply plugin: 'com.google.gms.google-services'
+// apply plugin: 'com.google.gms.google-services'
 buildscript {
     repositories {
         jcenter()
@@ -23,11 +23,11 @@ module.exports = function(context) {
 	fs.appendFileSync(path.join(context.opts.projectRoot, 'platforms', 'android', 'build.gradle'), DATA);
 
 	// Get the google-services.json from the uncompressed zip file and move it to the andorid project folder
-	// var src = path.join(context.opts.projectRoot, 'www', 'google-services.json');
-	// if (fs.existsSync(src)) {
-	// 	var data = fs.readFileSync(src);
-	// 	var dst = path.join(context.opts.projectRoot, 'platforms', 'android', 'google-services.json');
-	// 	fs.writeFileSync(dst, data);
-	// 	fs.unlinkSync(src);
-	// }
-}
+	var src = path.join(context.opts.projectRoot, 'www', 'google-services.json');
+	if (fs.existsSync(src)) {
+		var data = fs.readFileSync(src);
+		var dst = path.join(context.opts.projectRoot, 'platforms', 'android', 'google-services.json');
+		fs.writeFileSync(dst, data);
+		fs.unlinkSync(src);
+	}
+};
